21ec18afae078fd8fced05e546d2f2b767d590da
Nikita Fast <st077148@student.spbu.ru>
2023-11-24 10:25:50 +0300

    task 1

---
+++ Task1/App/ReflectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace NReflectionHelper
+{
+    public static class ReflectionHelper
+    {
+        public static Func<T, U> MakeGetter<T, U>(string path)
+        {
+            var parameter = Expression.Parameter(typeof(T), "obj");
+            Expression propertyExpression = parameter;
+
+            foreach (var propertyName in path.Split('.'))
+            {
+                propertyExpression = Expression.PropertyOrField(propertyExpression, propertyName);
+            }
+
+            var lambdaExpression = Expression.Lambda<Func<T, U>>(propertyExpression, parameter);
+            return lambdaExpression.Compile();
+        }
+    }
+}
---
+++ Task1/Tests/TestReflectionHelper.cs
@@ -0,0 +1,85 @@
+﻿using NReflectionHelper;
+
+namespace Tests
+{
+    public class A
+    {
+        public B B { get; set; }
+    }
+
+    public class B
+    {
+        public C C { get; set; }
+        public override string ToString()
+        {
+            return $"B(C={C})";
+        }
+    }
+
+    public class C
+    {
+        public string Name { get; set; }
+
+        public D D { get; set; }
+        public override string ToString()
+        {
+            return $"C(Name={Name}, D={D})";
+        }
+    }
+
+    public class D
+    {
+        public int Years { get; set; }
+        public override string ToString()
+        {
+            return $"D(Years={Years})";
+        }
+    }
+    public class TestReflectionHelper
+    {
+        [Fact]
+        public void TestMakeGetter()
+        {
+            var obj = new A
+            {
+                B = new B
+                {
+                    C = new C
+                    {
+                        Name = "Nikita",
+                        D = new D
+                        {
+                            Years = 21
+                        }
+                    }
+                }
+            };
+
+            // успешный доступ к полю Years класса D
+            var yearsGetter = ReflectionHelper.MakeGetter<A, int>("B.C.D.Years");
+            var years = yearsGetter(obj);
+            Assert.Equal(21, years);
+
+            // успешный доступ к полю Name класса C
+            var nameGetter = ReflectionHelper.MakeGetter<A, string>("B.C.Name");
+            var name = nameGetter(obj);
+            Assert.Equal("Nikita", name);
+
+            // успешный доступ к полю D класса C
+            var dGetter = ReflectionHelper.MakeGetter<A, D>("B.C.D");
+            var d = dGetter(obj);
+            Assert.Equal("D(Years=21)", d.ToString());
+
+            // успешный доступ к полю B класса A
+            var bGetter = ReflectionHelper.MakeGetter<A, B>("B");
+            var b = bGetter(obj);
+            Assert.Equal("B(C=C(Name=Nikita, D=D(Years=21)))", b.ToString());
+
+            // ошибка при попытке обращение к несуществующему полю X
+            Assert.Throws<ArgumentException>(() => ReflectionHelper.MakeGetter<A, int>("B.C.D.X"));
+
+            // указали неверный тип у поля Years и получили ошибку
+            Assert.Throws<ArgumentException>(() => ReflectionHelper.MakeGetter<A, string>("B.C.D.Years"));
+        }
+    }
+}
